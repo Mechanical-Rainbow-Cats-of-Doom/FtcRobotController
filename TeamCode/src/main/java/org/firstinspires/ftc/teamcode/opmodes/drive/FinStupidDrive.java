c62c7e1ba2ebfc120c4ad829b5fed54b96fe15cc
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2022-10-17 19:44:28 -0500

    robot is set up incorreclty aaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/drive/FinStupidDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/drive/FinStupidDrive.java
@@ -14,11 +14,11 @@ public class FinStupidDrive extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         waitForStart();
         while (opModeIsActive()) {
-            drive.setWeightedDrivePower(
+            drive.setDrivePower(
                     new Pose2d(
-                            -gamepad1.left_stick_y * 0.25,
-                            -gamepad1.left_stick_x * 0.25,
-                            -gamepad1.right_stick_x * 0.25
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
                     )
             );
             drive.update();
