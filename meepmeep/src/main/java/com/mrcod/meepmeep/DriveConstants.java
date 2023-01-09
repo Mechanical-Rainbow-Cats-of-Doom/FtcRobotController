dda2c4cb8917559fd26b756fdb53459eb86793a3
Patrick <psdecabooter@gmail.com>
2023-01-08 18:28:33 -0600

    fixed drive constants im MEEEEEPMOOOP

--- meepmeep/src/main/java/com/mrcod/meepmeep/DriveConstants.java
+++ meepmeep/src/main/java/com/mrcod/meepmeep/DriveConstants.java
@@ -20,8 +20,8 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 383.6;
-    public static final double MAX_RPM = 435;
+    public static final double TICKS_PER_REV = 537.7;
+    public static final double MAX_RPM = 312;
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -31,9 +31,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 1.88976; // in
-    public static double GEAR_RATIO = 0.5; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.32; // in
+    public static double WHEEL_RADIUS = 1.889765; // in
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 14.44; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -41,9 +41,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.0165;
+    public static double kA = 0.0031;
+    public static double kStatic = 0.001;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -71,10 +71,10 @@ public class DriveConstants {
      * Maximum Angular Velocity is calculated as: maximum velocity / trackWidth * (180 / Math.PI) but capped at 360°/s.
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      */
-    public static double MAX_VEL = 0.94 * 40.14618809757341;
-    public static double MAX_ACCEL = 73.17330064499293;
-    public static double MAX_ANG_VEL = Math.toRadians(232);
-    public static double MAX_ANG_ACCEL = Math.toRadians(360);
+    public static double MAX_VEL = 40;
+    public static double MAX_ACCEL = 40;
+    public static double MAX_ANG_VEL = 4;
+    public static double MAX_ANG_ACCEL = Math.toRadians(180);
 // 12.955587348170218 KF
 // 13.160383376726081 KF
 
