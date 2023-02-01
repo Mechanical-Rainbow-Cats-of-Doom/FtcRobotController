7eedd968178e344e1fbb122c6a44fcfe9c7036c0
LoganBurfield <cthulhumoose17@gmail.com>
2023-01-31 18:35:55 -0600

    they arent unlabled fin it literally says what they are and the long equation is from gobildas website

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/impl/auto/AutoTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/impl/auto/AutoTurret.java
@@ -35,8 +35,6 @@ public class AutoTurret {
     }
 
     protected final DcMotor motor;
-    // thanks ethan for the unlabeled magic numbers!
-    // well i assume it was you but you were on logans computer or something
     public static final double tpr = (((1+(46D/17))) * (1+(46D/11))) * 28 * 5; // 5 for gear
     public static final double ticksperdeg = tpr / 360;
     public static double offset = 0; // starting left corner
