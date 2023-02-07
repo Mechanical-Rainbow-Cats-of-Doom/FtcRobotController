06b28413bf9a6f6aebe7d03d53547fc7d7ad3556
ProfessorPiggos <doawk7@gmail.com>
2023-02-07 16:51:14 -0600

    edited code to be more code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/drive/FunnyControllerMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/drive/FunnyControllerMovement.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.core.robot.drive;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.util.Direction;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.core.robot.util.PoseStorage;
@@ -17,16 +18,17 @@ import androidx.annotation.NonNull;
 
 import static java.lang.Math.PI;
 
+@Config
 public class FunnyControllerMovement extends ControllerMovement {
     private boolean firstrun = true;
     public final static int AVG_COUNT_ACCELERATING = 10, AVG_COUNT_DECELERATING = 5;
-    final GamepadEx gamepad;
     private final BNO055IMU imu;
     private final Vector2d[] vals = new Vector2d[AVG_COUNT_ACCELERATING];
+    private final ToggleButtonReader bReader;
     private int loopCount = 0;
     public FunnyControllerMovement(@NonNull HardwareMap map, GamepadEx gamepad) {
         super(map, gamepad);
-        this.gamepad = gamepad;
+        bReader = new ToggleButtonReader(gamepad, GamepadKeys.Button.B);
         drive.setPoseEstimate(PoseStorage.currentPose);
         imu = map.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters() {{
@@ -50,7 +52,7 @@ public class FunnyControllerMovement extends ControllerMovement {
         final double y = gamepad.getLeftY();
         final double x = -gamepad.getLeftX();
         final Vector2d raw = new Vector2d(y, x);
-        if (firstrun)  {
+        if (firstrun) {
             Arrays.fill(vals, raw);
             firstrun = false;
         } else vals[loopCount % vals.length] = raw;
@@ -59,13 +61,16 @@ public class FunnyControllerMovement extends ControllerMovement {
             Arrays.fill(vals, (loopCount - AVG_COUNT_DECELERATING) % vals.length, loopCount % vals.length, avg);
             avg = calcAvg();
         }
-        Vector2d input = new Vector2d(avg.getY(), avg.getX()).rotated(imu.getAngularOrientation().firstAngle - PI / 2);
-        drive.setWeightedDrivePower(new Pose2d(
-                input.getX(),
-                input.getY(),
-                gamepad.getRightX()
-        ));
-        drive.updatePoseEstimate();
-        ++loopCount;
+        bReader.readValue();
+        if (bReader.getState()) {
+            Vector2d input = new Vector2d(avg.getY(), avg.getX()).rotated(imu.getAngularOrientation().firstAngle - PI / 2);
+            drive.setWeightedDrivePower(new Pose2d(
+                    input.getX(),
+                    input.getY(),
+                    gamepad.getRightX()
+            ));
+            drive.updatePoseEstimate();
+        } else super.update();
+        if (++loopCount % 10 == 0) loopCount = 0;
     }
 }
