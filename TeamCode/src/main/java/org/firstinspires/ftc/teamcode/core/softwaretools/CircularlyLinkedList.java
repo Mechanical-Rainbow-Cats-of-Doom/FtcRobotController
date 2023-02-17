86a86f956ec6c91fc1c959ef24018d0cd8e24e6e
ProfessorPiggos <doawk7@gmail.com>
2023-02-17 16:17:06 -0600

    Revert "feel free to revert if this breaks shit"
    This reverts commit 9190cbb72db42d0dbb1a5bd5445cd229b7a6ae74.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/softwaretools/CircularlyLinkedList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/softwaretools/CircularlyLinkedList.java
@@ -1,18 +1,12 @@
 package org.firstinspires.ftc.teamcode.core.softwaretools;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.LinkedList;
 
-public class CircularlyLinkedList<T> implements Serializable, Collection<T> {
+public class CircularlyLinkedList<T> implements Serializable {
     public static class Node<T> implements Serializable {
         private T val;
         private Node<T> nextNode;
-
         public Node(T val) {
             this.val = val;
         }
@@ -20,11 +14,11 @@ public class CircularlyLinkedList<T> implements Serializable, Collection<T> {
         public Node<T> getNextNode() {
             return nextNode;
         }
-
+        
         public void setNextNode(Node<T> nextNode) {
             this.nextNode = nextNode;
         }
-
+        
         public T getVal() {
             return val;
         }
@@ -33,7 +27,6 @@ public class CircularlyLinkedList<T> implements Serializable, Collection<T> {
             this.val = val;
         }
     }
-
     private final Node<T> head;
     private Node<T> curNode, tail;
     private final int capacity;
@@ -75,9 +68,7 @@ public class CircularlyLinkedList<T> implements Serializable, Collection<T> {
     public void set(int index, T val) {
         get(index).setVal(val);
     }
-
-    @Override
-    public boolean add(T val) {
+    public void add(T val) {
         if (size++ < capacity) {
             Node<T> newNode = new Node<>(val);
             tail.setNextNode(newNode);
@@ -87,98 +78,10 @@ public class CircularlyLinkedList<T> implements Serializable, Collection<T> {
             curNode.setVal(val);
             curNode = curNode.getNextNode();
         }
-        return true;
     }
 
-    @Override
-    public boolean remove(@Nullable Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean containsAll(@NonNull Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(@NonNull Collection<? extends T> c) {
-        c.forEach(this::add);
-        return true;
-    }
-
-    @Override
-    public boolean removeAll(@NonNull Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(@NonNull Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int size() {
+    public int length() {
         return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size != 0;
-    }
-
-    @Override
-    public boolean contains(@Nullable Object o) {
-        return this.stream().anyMatch(a -> a == o);
-    }
-
-    public static class ListIterator<T> implements Iterator<T> {
-        Node<T> current, head;
-        private boolean iterated = false;
-
-        public ListIterator(@NonNull CircularlyLinkedList<T> list) {
-            head = list.getHead();
-            current = head;
-        }
-
-        public ListIterator(Node<T> node) {
-            current = node;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return !iterated || current != head;
-        }
-
-        @Override
-        public T next() {
-            iterated = true;
-            T ret = current.getVal();
-            current = current.getNextNode();
-            return ret;
-        }
-    }
-
-    @NonNull
-    @Override
-    public Iterator<T> iterator() {
-        return new ListIterator<>(this);
-    }
-
-    @NonNull
-    @Override
-    public Object[] toArray() {
-        return new ArrayList<>(this).toArray();
-    }
-
-    @NonNull
-    @Override
-    public <T1> T1[] toArray(@NonNull T1[] a) {
-        throw new UnsupportedOperationException(); // this is bad
     }
 
 }
